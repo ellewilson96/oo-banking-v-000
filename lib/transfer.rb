4ed5a328e288f2a932d86f88fcddf122aeca1a78
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-06 16:51:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,24 +15,28 @@ end
     sender.valid? && receiver.valid?
 end
 
-  def execute_transaction
-    if valid? == true
-      @sender.balance -= @amount && @receiver.balance = @amount
-      @status == "complete"
-    else
-      reject_transfer
-    end
+def execute_transaction
+  if valid? && sender.balance > amount && self.status == "pending"
+    sender.balance -= amount
+    receiver.balance += amount
+    self.status = "complete"
+  else
+    reject_transfer
   end
+end
 
-  def reverse_transfer
-    if @status == "rejected"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-    end
+def reverse_transfer
+  if valid? && receiver.balance > amount && self.status == "complete"
+    receiver.balance -= amount
+    sender.balance += amount
+    self.status = "reversed"
+  else
+    reject_transfer
   end
+end
 
   def reject_transfer
-    @status == "rejected"
+    self.status == "rejected"
     puts "Transaction rejected. Please check your account balance."
   end
 end
